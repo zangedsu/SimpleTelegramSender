0fd84a66ca54deffba154a5a42517519ed8eb06d
Vitaliy <zangedsu@gmail.com>
2023-04-19 19:00:17 +0300

    Исправил ошибку в примере

--- UsageExample/Program.cs
+++ UsageExample/Program.cs
@@ -1,7 +1,7 @@
 ﻿using SimpleTelegramSender;
 try
 {
-    var sender = new Sender("TG_BOT_HERE", 111);
+    var sender = new Sender("TG_BOT_TOKEN_HERE", 111);
     sender.SendMessageAsync("Hello World!!");
 }
 catch(Exception ex) { Console.WriteLine(ex.Message); }
