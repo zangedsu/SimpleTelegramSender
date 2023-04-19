8012f467387089a16e3bd3c2b818b520bd316612
Vitaliy <zangedsu@gmail.com>
2023-04-19 18:47:18 +0300

    Добавьте файлы проекта.

---
+++ SimpleTelegramSender/Sender.cs
@@ -0,0 +1,27 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+
+namespace SimpleTelegramSender;
+
+    public class Sender
+    {
+    private TelegramBotClient _botClient;
+    private long _chatId;
+    private CancellationTokenSource _ct = new();
+
+    public Sender(string token, long chatId)
+    {
+        _botClient = new TelegramBotClient(token);
+        _chatId = chatId;
+    }
+
+    public async Task SendMessageAsync(string message)
+    {
+        await _botClient.SendTextMessageAsync(
+            chatId: _chatId,
+            text: message,
+            cancellationToken: _ct.Token);
+    }
+}
---
+++ UsageExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using SimpleTelegramSender;
+using System.Threading;
+try
+{
+    var sender = new Sender("6246641246:AAGPQolVse2Ps_2jGFDOvTLwW6zEC0Dp6dU", 494650613);
+    Thread.Sleep(1000);
+    sender.SendMessageAsync("Hello World!!");
+    
+
+}
+catch(Exception ex) { Console.WriteLine(ex.Message); }
+Console.ReadLine();
